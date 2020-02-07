0f23c27ff46a539375c6ba70797e8775c118be62
Davies Fatimah Omowonuola <daviesfatimah97@gmail.com>
2020-02-07 15:14:23 -0800

    json data

---
+++ json.js
@@ -0,0 +1,56 @@
+const jsondata=[
+    {
+        "name":"Backpropagation",
+        "meaning":"is a method used in artificial neural networks to calculate a gradient that is needed in the calculation of the weights to be used in the network.[38] Backpropagation is shorthand for 'the backward propagation of errors,' since an error is computed at the output and distributed backwards throughout the network's layers. It is commonly used to train deep neural networks,[39] a term referring to neural networks with more than one hidden layer"
+    },
+    {
+        "name":"Artificial neural network (ANN)",
+        "meaning":"A learning model created to act like a human brain that solves tasks that are too difficult for traditional computer systems to solve."
+    },
+    {
+        "name":"Clustering",
+        "meaning":"Clustering algorithms let machines group data points or items into groups with similar characteristics."
+    },
+    {
+        "name":"Deep learning",
+        "meaning":"The ability for machines to autonomously mimic human thought patterns through artificial neural networks composed of cascading layers of information"
+    },
+]
+
+const jsondataweb=[
+    {
+        "name":"Uniform Resource Locators ",
+        "meaning":"URLs — are the web browser addresses of internet pages and files. With a URL, you can locate and bookmark specific pages and files in your web browser. URLs can be found all around us. They may be listed at the bottom of business cards, on TV screens during commercial breaks, linked in documents you read on the internet or delivered by one of the internet search engines"
+    },
+    {
+        "name":"HTTP ",
+        "meaning":"is the acronym for 'Hypertext Transfer Protocol,' the data communication standard of web pages. When a web page has this prefix, the links, text, and pictures should work correctly in your web browser."
+    },
+    {
+        "name":"HTTPS",
+        "meaning":"is the acronym for 'Hypertext Transfer Protocol Secure.' This indicates that the web page has a special layer of encryption added to hide your personal information and passwords from others. Whenever you log in to your online bank account or a shopping site that you enter credit card information into, look for 'https' in the URL for security."
+    },
+    {
+        "name":"XML",
+        "meaning":" is eXtensible Markup Language, a cousin to HTML. XML focuses on cataloging and databasing the text content of a web page."
+    },
+]
+
+const jsondatanetwork=[
+    {
+        "name":"ARM",
+        "meaning":"Asynchronous response mode, HDLC communication mode involving one primary station and at least one secondary station, where either the primary or one of the secondary stations can initiate transmissions. See also primary station and secondary station."
+    },
+    {
+        "name":"Adapter",
+        "meaning":"Hardware that allows a computing device physical access to a network."
+    },
+    {
+        "name":"Address Mask",
+        "meaning":"Bit combination used to describe which portion of anaddress refers to the network or subnet and which partrefers to the host. Sometimes referred to simply as mask.also subnet mask."
+    },
+    {
+        "name":"API",
+        "meaning":"Application Programming Interface. A set of tools and procedures provided by the programmer of an application so that other programmers can control, exchange data with, or extend the functionality of an application."
+    },
+]
